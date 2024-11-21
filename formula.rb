3a7cb51601c9fd27b9dde486c77a4daa97147035
Niklas <niklas.hanft@gmail.com>
2024-11-21 21:19:36 +0100

    Update

--- formula.rb
+++ formula.rb
@@ -1,16 +1,10 @@
-cask "nfs" do
-  version "4.0.12"
+class Foo < Formula
+  version "0.0.1-alpha"
   sha256 "a4e1a30d074e724ba24e9e2674a72bc4050f00161fb7dc23295a2c189ecda5bb"
 
-  url "https://github.com/nhh/nfs/releases/download/v#{version}/nfs-#{version}.dmg"
+  url "https://github.com/nhh/nfs/releases/download/v#{version}/#{version}/nfs-linux-x86_64"
   name "Need for Sync"
   desc "Sync files to your k8s pods"
   
   homepage "https://github.com/nhh/nfs"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
 end
